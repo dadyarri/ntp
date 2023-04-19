6ef7b0b9b599b6025e5643671a3ee84c38512cdf
Daniil <dadyarri@gmail.com>
2023-04-19 14:02:42 +0300

    feat: add some basic design for bookmark component

--- src/shared/ui/bookmark/index.tsx
+++ src/shared/ui/bookmark/index.tsx
@@ -1,3 +1,15 @@
-export const Bookmark = () => {
-    return <></>
+import {Box, Typography} from "@mui/material";
+import {FC} from "react";
+import {Bookmark as BookmarkType} from "../../../entities/bookmark";
+
+interface BookmarkProps {
+    bookmark: BookmarkType;
+}
+
+export const Bookmark: FC<BookmarkProps> = ({bookmark}) => {
+    return <Box sx={{border: 1, borderRadius: "14px", padding: "10px", cursor: "pointer", userSelect: "none"}}>
+        <Typography sx={{fontSize: "16px"}}>
+            {bookmark.title}
+        </Typography>
+    </Box>
 };
