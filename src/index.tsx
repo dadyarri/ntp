7ed7060386d58ec8db289739c68e8d1a38b2f5c5
Daniil <dadyarri@gmail.com>
2023-04-19 18:00:17 +0300

    feat: add using system theme

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,24 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {createTheme, ThemeProvider, useMediaQuery} from "@mui/material";
 
+const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+const theme = React.useMemo(
+    () =>
+        createTheme({
+            palette: {
+                mode: prefersDarkMode ? 'dark' : 'light',
+            },
+        }),
+    [prefersDarkMode],
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
 )
