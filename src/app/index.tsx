25de31f274c155597e8b12ba6cc73d99043c4fba
Daniil <dadyarri@gmail.com>
2023-04-19 10:49:00 +0300

    feat: add container with date & time

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,29 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import {Container, Typography} from "@mui/material";
+import {DateTime} from "luxon";
 
 function Index() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <></>
-  )
+    const [dateTime, setDateTime] = useState(DateTime.now());
+
+    useEffect(() => {
+        setTimeout(
+            () => setDateTime(DateTime.now()),
+            1000
+        );
+    }, [dateTime])
+
+    return (
+        <Container maxWidth={"md"} sx={{
+            minHeight: "100px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center"
+        }}>
+            <Typography variant={"h3"}>{dateTime.toFormat("hh:mm:ss")}</Typography>
+            <Typography variant={"h3"}>{dateTime.toFormat("dd.MM.yyyy")}</Typography>
+        </Container>
+    );
 }
 
 export default Index
