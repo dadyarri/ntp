fbaafdb8fa7ddbe8a9416bfa9e1d94631c50dcd5
Daniil <dadyarri@gmail.com>
2023-04-19 13:33:09 +0300

    feat: remove seconds; add working with chrome storage; add mapping on bookmarks

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,50 @@
 import {useEffect, useState} from 'react'
 import {Container, Typography} from "@mui/material";
 import {DateTime} from "luxon";
+import {Bookmark as BookmarkType} from "../entities/bookmark";
 
 function Index() {
 
     const [dateTime, setDateTime] = useState(DateTime.now());
+    const [bookmarks, setBookmarks] = useState<BookmarkType[]>([]);
 
     useEffect(() => {
         setTimeout(
             () => setDateTime(DateTime.now()),
-            1000
+            60000
         );
-    }, [dateTime])
+
+        chrome.storage.local.get(null).then((data) => {
+            if (data.bookmarks === undefined) {
+                let bookmarks: BookmarkType[] = [];
+                chrome.storage.local.set({bookmarks: bookmarks})
+                data.bookmarks = bookmarks;
+            }
+
+            setBookmarks(data.bookmarks);
+
+        });
+
+    }, []);
 
     return (
-        <Container maxWidth={"md"} sx={{
-            minHeight: "100px",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center"
-        }}>
-            <Typography variant={"h3"}>{dateTime.toFormat("hh:mm:ss")}</Typography>
-            <Typography variant={"h3"}>{dateTime.toFormat("dd.MM.yyyy")}</Typography>
-        </Container>
+        <>
+            <Container maxWidth={"md"} sx={{
+                minHeight: "100px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center"
+            }}>
+                <Typography variant={"h3"}>{dateTime.toFormat("HH:mm")}</Typography>
+                <Typography variant={"h3"}>{dateTime.toFormat("dd.MM.yyyy")}</Typography>
+            </Container>
+
+
+            {bookmarks.map((bookmark: any) => (
+                // <Bookmark/>
+                <Typography>{bookmark.title}</Typography>
+            ))}
+        </>
     );
 }
 
