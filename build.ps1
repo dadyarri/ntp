9f5647f75405317021cf7f2b14bb39abdd745ae0
Daniil <dadyarri@gmail.com>
2023-04-20 10:46:58 +0300

    refactor: remove unused script

--- build.ps1
+++
@@ -1,9 +0,0 @@
-npx next build
-npx next export
-mv out\_next out\next
-(Get-ChildItem out -Filter *.html -Recurse) | ForEach-Object {
-    (Get-Content $_.FullName) | ForEach-Object {
-        $_ -replace "/_next", "./next"
-    } | Set-Content $_.FullName
-}
-Copy-Item -Path "out\*" -Destination "C:\Users\dadya\.config\ntp" -Recurse -Force
