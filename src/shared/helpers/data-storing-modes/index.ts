e5b0c583d9b55f7df1c25fa6aa34050a89ef2617
Daniil <dadyarri@gmail.com>
2023-04-20 23:15:20 +0300

    feat: add utils to convert between modes, and check if data is in plain mode

---
+++ src/shared/helpers/data-storing-modes/index.ts
@@ -0,0 +1,31 @@
+import {Bookmark} from "../../../entities/bookmark";
+import {Folder} from "../../../entities/folder";
+
+export const isInPlainMode = (data: (Bookmark | Folder)[]): boolean => {
+
+    return data.every((item): item is Bookmark => {
+        return (item as Bookmark).url !== undefined && (item as Bookmark).title !== undefined;
+    });
+};
+
+export const convertToPlainMode = (folders: Folder[]): Bookmark[] => {
+    const bookmarks: Bookmark[] = [];
+
+    folders.forEach(folder => {
+        folder.bookmarks.forEach(bookmark => {
+            bookmarks.push(bookmark);
+        });
+    });
+
+    return bookmarks;
+};
+
+export const convertToComplexMode = (bookmarks: Bookmark[]): Folder[] => {
+    const folder: Folder = {
+        id: 1,
+        name: "Converted",
+        bookmarks: bookmarks
+    };
+
+    return [folder];
+};
