8c5069df2f8feff92e82439e2570809ad0cb6990
Daniil <dadyarri@gmail.com>
2023-04-21 14:35:50 +0300

    refactor: allow bookmarks to be folder type

--- src/shared/contexts/bookmarks.ts
+++ src/shared/contexts/bookmarks.ts
@@ -1,8 +1,9 @@
 import React, {createContext} from "react";
 import {Bookmark as BookmarkType} from "../../entities/bookmark";
+import {Folder as FolderType} from "../../entities/folder";
 
 export const BookmarksContext = createContext<{
-    bookmarks: BookmarkType[],
-    setBookmarks: React.Dispatch<React.SetStateAction<BookmarkType[]>> | null,
+    bookmarks: BookmarkType[] | FolderType[],
+    setBookmarks: React.Dispatch<React.SetStateAction<BookmarkType[] | FolderType[]>> | null,
     editMode: boolean
 }>({bookmarks: [], setBookmarks: null, editMode: true});
